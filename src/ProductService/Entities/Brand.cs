d3a01df46e41e0f9972eb964ae8dc4697d9335c7
Wale <onafesowale@gmail.com>
2023-10-09 12:46:00 +0100

    Entities

---
+++ src/ProductService/Entities/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductService.Entities;
+
+public class Brand
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<Product> Products { get; set; }
+}
---
+++ src/ProductService/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductService.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int YearOfRelease { get; set; }
+    public string ImageUrl { get; set; }
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
+    public Guid BrandId { get; set; }
+    public Brand Brand { get; set; }
+}
