baf6fb4cda186ed1396d713f3f1009a88ab3a45a
Wale <onafesowale@gmail.com>
2023-10-09 12:46:28 +0100

    Endpoint definitions

---
+++ src/ProductService/Definitions/BrandEndpointDefinition.cs
@@ -0,0 +1,59 @@
+﻿using MediatR;
+using ProductService.Definitions.Contracts;
+using ProductService.Features.Brand.Command.Create;
+using ProductService.Features.Brand.Queries.GetBrandById;
+using ProductService.Features.Brand.Queries.GetBrands;
+using ProductService.Request;
+
+namespace ProductService.Definitions;
+
+public class BrandEndpointDefinition : IEndpointDefinition
+{
+    public void RegisterEndpoints(WebApplication app)
+    {
+        const string TAGNAME = "Brands";
+
+        var brand = app.MapGroup("/api/brands");
+
+        brand.MapGet("", GetBrandsAsync)
+            .WithTags(TAGNAME);
+
+        brand.MapPost("", CreateBrandAsync)
+            .WithTags(TAGNAME);
+
+        brand.MapGet("/{id}", GetBrandAsync)
+            .WithName("GetBrandById")
+            .WithTags(TAGNAME);
+    }
+
+    private async Task<IResult> GetBrandsAsync(IMediator mediator)
+    {
+        var request = new GetBrandsRequest();
+
+        var results = await mediator.Send(request);
+
+        return Results.Ok(results);
+    }
+
+    private async Task<IResult> CreateBrandAsync(IMediator mediator, CreateBrandRequest brand)
+    {
+        var request = new CreateBrandCommand
+        {
+            Id = Guid.Parse(brand.Id),
+            Name = brand.Name
+        };
+
+        var id = await mediator.Send(request);
+
+        return Results.CreatedAtRoute("GetBrandById", id, request);
+    }
+
+    private async Task<IResult> GetBrandAsync(IMediator mediator, Guid id)
+    {
+        var request = new GetBrandByIdRequest { Id = id };
+
+        var result = await mediator.Send(request);
+
+        return TypedResults.Ok(result);
+    }
+}
---
+++ src/ProductService/Definitions/ProductEndpointDefinition.cs
@@ -0,0 +1,65 @@
+﻿using MediatR;
+using ProductService.Definitions.Contracts;
+using ProductService.Features.Product.Commands.Create;
+using ProductService.Features.Product.Queries.GetProductById;
+using ProductService.Features.Product.Queries.GetProducts;
+using ProductService.Request;
+
+namespace ProductService.Definitions;
+
+public class ProductEndpointDefinition : IEndpointDefinition
+{
+    public void RegisterEndpoints(WebApplication app)
+    {
+        var product = app.MapGroup("/api/products");
+
+        product.MapGet("", GetProducts)
+            .WithTags("Products");
+
+        product.MapGet("/{id}", GetProductById)
+            .WithTags("Products")
+            .WithName("GetProductById");
+
+        product.MapPost("", CreateProduct)
+            .WithTags("Products");
+
+    }
+
+    private async Task<IResult> GetProducts(IMediator mediator)
+    {
+        var request = new GetProductsRequest();
+
+        var result = await mediator.Send(request);
+
+        return Results.Ok(result);
+    }
+
+    private async Task<IResult> GetProductById(IMediator mediator,  Guid id)
+    {
+        var request = new GetProductByIdRequest { Id = id };
+
+        var result = await mediator.Send(request);
+
+        return Results.Ok(result);
+    }
+
+    private async Task<IResult> CreateProduct(IMediator mediator, CreateProductRequest product)
+    {
+        var request = new CreateProductCommand
+        {
+            Id = Guid.Parse(product.Id),
+            Name = product.Name,
+            Description = product.Description,
+            ImageUrl = product.ImageUrl,
+            BrandId = Guid.Parse(product.BrandId),
+            Price = product.Price,
+            CategoryId = Guid.Parse(product.CategoryId),
+            YearOfRelease = product.YearOfRelease
+        };
+
+        var result = await mediator.Send(request);
+
+        return Results.CreatedAtRoute("GetProductById", result, request);
+
+    }
+}
