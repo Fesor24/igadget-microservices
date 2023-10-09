110a0a332edc649981d61fd99748c82e430973bd
Wale <onafesowale@gmail.com>
2023-10-09 12:45:51 +0100

    Endpoint extensions

---
+++ src/ProductService/Extensions/EndpointExtension.cs
@@ -0,0 +1,19 @@
+﻿using ProductService.Definitions.Contracts;
+
+namespace ProductService.Extensions;
+
+public static class EndpointExtension
+{
+    public static void RegisterEndpoints(this WebApplication app)
+    {
+        var endpointDefinitions = typeof(Program).Assembly.GetTypes()
+            .Where(x => x.IsAssignableTo(typeof(IEndpointDefinition)) && !x.IsAbstract && !x.IsInterface)
+            .Select(Activator.CreateInstance)
+            .Cast<IEndpointDefinition>();
+
+        foreach(var endpointDefinition in endpointDefinitions)
+        {
+            endpointDefinition.RegisterEndpoints(app);
+        }
+    }
+}
