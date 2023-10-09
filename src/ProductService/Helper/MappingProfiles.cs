d5394b5091799aff5b582310ee08af7263c7c14f
Wale <onafesowale@gmail.com>
2023-10-09 12:42:51 +0100

    mapping profiles

---
+++ src/ProductService/Helper/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductService.Entities;
+using ProductService.Response;
+
+namespace ProductService.Helper;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Product, GetProductResponse>()
+            .ForMember(x => x.CategoryName, o => o.MapFrom(t => t.Category.Name));
+
+        CreateMap<Brand, GetBrandResponse>();
+    }
+}
