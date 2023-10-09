94990ec0d6c7d555707f87092616fcb9be58eccf
Wale <onafesowale@gmail.com>
2023-10-09 12:43:47 +0100

    Request objects

---
+++ src/ProductService/Request/CreateBrandRequest.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using ProductService.Helper;
+
+namespace ProductService.Request;
+
+public class CreateBrandRequest
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class CreateBrandRequestValidator : AbstractValidator<CreateBrandRequest>
+{
+    public CreateBrandRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("Name can not be null or empty");
+
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Id can not be null or empty");
+
+        RuleFor(x => x.Id)
+            .Must(CustomValidators.IsValidGuid)
+            .WithMessage("Value(Brand Id) must be a valid Guid");
+    }
+}
---
+++ src/ProductService/Request/CreateProductRequest.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using ProductService.Helper;
+
+namespace ProductService.Request;
+
+public class CreateProductRequest
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int YearOfRelease { get; set; }
+    public string ImageUrl { get; set; }
+    public string CategoryId { get; set; }
+    public string BrandId { get; set; }
+}
+
+public class CreateProductRequestValidator : AbstractValidator<CreateProductRequest>
+{
+    public CreateProductRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Name can not be null or empty");
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Description can not be null or empty");
+
+        RuleFor(x => x.ImageUrl)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Name can not be null or empty");
+
+        RuleFor(x => x.Id)
+            .Must(CustomValidators.IsValidGuid)
+            .WithMessage("Pass in a valid Guid -- Product Id");
+
+        RuleFor(x => x.BrandId)
+            .Must(CustomValidators.IsValidGuid)
+            .WithMessage("Pass in a valid Guid -- Brand Id");
+
+        RuleFor(x => x.CategoryId)
+            .Must(CustomValidators.IsValidGuid)
+            .WithMessage("Pass in a valid Guid -- Category id");
+
+    }
+}
