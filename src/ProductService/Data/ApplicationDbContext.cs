40e0e93065fc2c489f79d1fc9937f0ddab31bcb8
Wale <onafesowale@gmail.com>
2023-10-09 12:47:04 +0100

    Applicationdb context

---
+++ src/ProductService/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Entities;
+
+namespace ProductService.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
