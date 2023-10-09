e57e53d797ccd12c96d3c48ba0f58ff8dfe99f49
Wale <onafesowale@gmail.com>
2023-10-09 12:44:15 +0100

    GetProductById functionality

---
+++ src/ProductService/Features/Product/Queries/GetProductById/GetProductByIdRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProductService.Response;
+
+namespace ProductService.Features.Product.Queries.GetProductById;
+
+public class GetProductByIdRequest : IRequest<GetProductResponse>
+{
+    public Guid Id { get; set; }
+}
