f280c2a911d31d1c7a174b1f03d4567865e7a024
Wale <onafesowale@gmail.com>
2023-10-09 19:46:21 +0100

    Get Categories functionality

---
+++ src/ProductService/Features/Category/Queries/GetCategories/GetCategoriesRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using ProductService.Response;
+
+namespace ProductService.Features.Category.Queries.GetCategories;
+
+public class GetCategoriesRequest : IRequest<IReadOnlyList<GetCategoryResponse>>
+{
+}
---
+++ src/ProductService/Features/Category/Queries/GetCategories/GetCategoriesRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using ProductService.DataAccess.Contracts;
+using ProductService.Response;
+
+namespace ProductService.Features.Category.Queries.GetCategories;
+
+public class GetCategoriesRequestHandler : IRequestHandler<GetCategoriesRequest, IReadOnlyList<GetCategoryResponse>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetCategoriesRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<GetCategoryResponse>> Handle(GetCategoriesRequest request, CancellationToken cancellationToken)
+    {
+        var categories = await _unitOfWork.CategoryRepository.GetAllAsync();
+
+        return _mapper.Map<IReadOnlyList<GetCategoryResponse>>(categories);
+    }
+}
