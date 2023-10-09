c83dcff3bd6940fcfa381fe41ebd5cd07048bd4c
Wale <onafesowale@gmail.com>
2023-10-09 12:44:03 +0100

    Get products functionality

---
+++ src/ProductService/Features/Product/Queries/GetProducts/GetProductsRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using ProductService.Response;
+
+namespace ProductService.Features.Product.Queries.GetProducts;
+
+public class GetProductsRequest : IRequest<IReadOnlyList<GetProductResponse>>
+{
+}
---
+++ src/ProductService/Features/Product/Queries/GetProducts/GetProductsRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using ProductService.DataAccess.Contracts;
+using ProductService.Response;
+
+namespace ProductService.Features.Product.Queries.GetProducts;
+
+public class GetProductsRequestHandler : IRequestHandler<GetProductsRequest, IReadOnlyList<GetProductResponse>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetProductsRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<GetProductResponse>> Handle(GetProductsRequest request, CancellationToken cancellationToken)
+    {
+        var products = await _unitOfWork.ProductRepository.GetAllAsync();
+
+        return _mapper.Map<IReadOnlyList<GetProductResponse>>(products);
+    }
+}
