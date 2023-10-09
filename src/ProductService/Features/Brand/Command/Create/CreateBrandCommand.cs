d9cc4abcce7e77eaa1c16749f913383fb9944b98
Wale <onafesowale@gmail.com>
2023-10-09 12:44:43 +0100

    Create brand functionality

---
+++ src/ProductService/Features/Brand/Command/Create/CreateBrandCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ProductService.Features.Brand.Command.Create;
+
+public class CreateBrandCommand : IRequest<Guid>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ src/ProductService/Features/Brand/Command/Create/CreateBrandCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using ProductService.DataAccess.Contracts;
+using BrandEntity = ProductService.Entities.Brand;
+
+namespace ProductService.Features.Brand.Command.Create;
+
+public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, Guid>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateBrandCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Guid> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    {
+        BrandEntity brand = new()
+        {
+            Id = request.Id,
+            Name = request.Name
+        };
+
+        await _unitOfWork.BrandRepository.AddAsync(brand);
+
+        return brand.Id;
+    }
+}
