5ca5ef26dd7f52598b2ab8c097b10c31635827ca
Wale <onafesowale@gmail.com>
2023-10-09 19:43:58 +0100

    Added mappings

--- src/ProductService/Helper/MappingProfiles.cs
+++ src/ProductService/Helper/MappingProfiles.cs
@@ -9,8 +9,11 @@ public class MappingProfiles : Profile
     public MappingProfiles()
     {
         CreateMap<Product, GetProductResponse>()
-            .ForMember(x => x.CategoryName, o => o.MapFrom(t => t.Category.Name));
+            .ForMember(x => x.Category, o => o.MapFrom(t => t.Category.Name))
+            .ForMember(x => x.Brand, o => o.MapFrom(t => t.Brand.Name));
 
         CreateMap<Brand, GetBrandResponse>();
+
+        CreateMap<Category, GetCategoryResponse>();
     }
 }
