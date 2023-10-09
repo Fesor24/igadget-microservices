58fdf92239951e006b560876c3b5f1b144334c9f
Wale <onafesowale@gmail.com>
2023-10-09 12:44:32 +0100

    Create product functionality

---
+++ src/ProductService/Features/Product/Commands/Create/CreateProductCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace ProductService.Features.Product.Commands.Create;
+
+public class CreateProductCommand : IRequest<Guid>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int YearOfRelease { get; set; }
+    public string ImageUrl { get; set; }
+    public Guid CategoryId { get; set; }
+    public Guid BrandId { get; set; }  
+}
---
+++ src/ProductService/Features/Product/Commands/Create/CreateProductCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using ProductService.DataAccess.Contracts;
+using ProductEntity = ProductService.Entities.Product;
+
+namespace ProductService.Features.Product.Commands.Create;
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, Guid>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateProductCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Guid> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        ProductEntity product = new()
+        {
+            Name = request.Name,
+            Description = request.Description,
+            CategoryId = request.CategoryId,
+            YearOfRelease = request.YearOfRelease,
+            ImageUrl = request.ImageUrl,
+            Price = request.Price,
+            Id = request.Id,
+            BrandId = request.BrandId
+        };
+
+        await _unitOfWork.ProductRepository.AddAsync(product);
+
+        await _unitOfWork.Complete();
+
+        return product.Id;
+    }
+}
