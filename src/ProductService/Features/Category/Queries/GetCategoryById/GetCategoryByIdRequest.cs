f93beab8cc97f8c19545f8f3db09e3115e7d8467
Wale <onafesowale@gmail.com>
2023-10-09 19:46:34 +0100

    Get category by id functionality

---
+++ src/ProductService/Features/Category/Queries/GetCategoryById/GetCategoryByIdRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProductService.Response;
+
+namespace ProductService.Features.Category.Queries.GetCategoryById;
+
+public class GetCategoryByIdRequest  :IRequest<GetCategoryResponse>
+{
+    public Guid Id { get; set; }
+}
---
+++ src/ProductService/Features/Category/Queries/GetCategoryById/GetCategoryByIdRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using ProductService.DataAccess.Contracts;
+using ProductService.Response;
+using Shared.Exceptions;
+
+namespace ProductService.Features.Category.Queries.GetCategoryById;
+
+public class GetCategoryByIdRequestHandler : IRequestHandler<GetCategoryByIdRequest, GetCategoryResponse>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetCategoryByIdRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+    public async Task<GetCategoryResponse> Handle(GetCategoryByIdRequest request, CancellationToken cancellationToken)
+    {
+        var category = await _unitOfWork.CategoryRepository.GetAsync(request.Id);
+
+        return category is null ?
+            throw new ApiNotFoundException($"Category with Id: {request.Id} not found") :
+            _mapper.Map<GetCategoryResponse>(category);
+    }
+}
