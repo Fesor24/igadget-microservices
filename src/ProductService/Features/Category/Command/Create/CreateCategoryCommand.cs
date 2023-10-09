70920afed66eac0f496cbbbd535942f019716c28
Wale <onafesowale@gmail.com>
2023-10-09 19:46:00 +0100

    Create category functionality

---
+++ src/ProductService/Features/Category/Command/Create/CreateCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ProductService.Features.Category.Command.Create;
+
+public class CreateCategoryCommand : IRequest<Guid>
+{
+    public string Name { get; set; }    
+}
---
+++ src/ProductService/Features/Category/Command/Create/CreateCategoryCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using ProductService.DataAccess.Contracts;
+using CategoryEntity =  ProductService.Entities.Category;
+
+namespace ProductService.Features.Category.Command.Create;
+
+public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, Guid>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateCategoryCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Guid> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        CategoryEntity category = new()
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name
+        };
+
+        await _unitOfWork.CategoryRepository.AddAsync(category);
+
+        await _unitOfWork.Complete();
+
+        return category.Id;
+    }
+}
