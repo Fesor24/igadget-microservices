20f3fbeccd349d63447b0b79e83543410c18adf1
Wale <onafesowale@gmail.com>
2023-10-09 19:44:40 +0100

    Added methods

---
+++ src/ProductService/DataAccess/Contracts/ICategoryRepository.cs
@@ -0,0 +1,7 @@
+﻿using ProductService.Entities;
+
+namespace ProductService.DataAccess.Contracts;
+
+public interface ICategoryRepository : IGenericRepository<Category>
+{
+}
--- src/ProductService/DataAccess/Contracts/IProductRepository.cs
+++ src/ProductService/DataAccess/Contracts/IProductRepository.cs
@@ -5,4 +5,5 @@ namespace ProductService.DataAccess.Contracts;
 public interface IProductRepository : IGenericRepository<Product>
 {
     Task<Product> GetProductDetails(Guid id);
+    Task<IReadOnlyList<Product>> GetProductsDetails();
 }
