3fa47ff266da45995a5a35af5994fff87ce71471
Wale <onafesowale@gmail.com>
2023-10-09 19:42:49 +0100

    ProductDbContext

---
+++ src/ProductService/Data/ProductDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Entities;
+
+namespace ProductService.Data;
+
+public class ProductDbContext : DbContext
+{
+    public ProductDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+
+    public DbSet<Brand> Brands => Set<Brand>();
+
+    public DbSet<Category> Categories => Set<Category>();
+
+}
