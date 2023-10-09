cc470cc404866f7e459c384833e56f935e239b38
Wale <onafesowale@gmail.com>
2023-10-09 12:44:55 +0100

    Application extensions

---
+++ src/ProductService/Extensions/ApplicationExtension.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using ProductService.Data;
+using ProductService.DataAccess.Contracts;
+using ProductService.DataAccess.Repository;
+
+namespace ProductService.Extensions;
+
+public static class ApplicationExtension
+{
+    public static void AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<ApplicationDbContext>(opt =>
+        {
+            opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddMediatR(opt =>
+        {
+            opt.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+        });
+
+        services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        
+    }
+}
