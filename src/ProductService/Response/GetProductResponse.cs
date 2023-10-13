2f352ab1c5d9cf288ad65b8e4c441408aa64fcb4
Wale <onafesowale@gmail.com>
2023-10-13 08:54:55 +0100

    Added YearofRelease prop

--- src/ProductService/Response/GetProductResponse.cs
+++ src/ProductService/Response/GetProductResponse.cs
@@ -6,6 +6,7 @@ public class GetProductResponse
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
+    public int YearOfRelease { get; set; }
     public string ImageUrl { get; set; }
     public string Category { get; set; }
     public string Brand { get; set; }
