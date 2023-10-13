5ff06bce0a5efb77e89d77113f44386838d1d4fe
Wale <onafesowale@gmail.com>
2023-10-13 08:54:40 +0100

    search db setup

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,6 +1,19 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await DB.InitAsync("Search", MongoClientSettings.FromConnectionString(
+    app.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Product>()
+    .Key(x => x.Name, KeyType.Text)
+    .Key(x => x.BrandName, KeyType.Text)
+    .Key(x => x.CategoryName, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
