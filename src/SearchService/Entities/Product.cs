e5a38c64e6288b7529fc4470b66893568ee3f5cf
Wale <onafesowale@gmail.com>
2023-10-13 08:55:05 +0100

    Product entity

---
+++ src/SearchService/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService.Entities;
+
+public class Product : Entity
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int YearOfRelease { get; set; }
+    public string ImageUrl { get; set; }
+    public string CategoryName { get; set; }
+    public string BrandName { get; set; }
+}
