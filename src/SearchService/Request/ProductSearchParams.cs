98aaf9144779a7a5e14c4cffa8d62c7ad14bed4b
Wale <onafesowale@gmail.com>
2023-10-14 20:43:01 +0100

    product search params

---
+++ src/SearchService/Request/ProductSearchParams.cs
@@ -0,0 +1,14 @@
+﻿namespace SearchService.Request;
+
+public record ProductSearchParams
+{
+    public string SearchTerm { get; set; }
+    public int PageSize { get; set; } = 10;
+    public int PageNumber { get; set; } = 1;
+    public int YearOfReleaseStart { get; set; }
+    public int YearOfReleaseEnd { get; set; }
+    public decimal MinimumPrice { get; set; }
+    public decimal MaximumPrice { get; set; }
+    public string SortBy { get; set; }
+    public string SortDirection { get; set; }
+}
