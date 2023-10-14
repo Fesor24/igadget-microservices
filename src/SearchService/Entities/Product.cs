afe7f7c1a2edcf73b6d5659dcf0d0c9e61880db0
Wale <onafesowale@gmail.com>
2023-10-14 15:50:40 +0100

    modified props

--- src/SearchService/Entities/Product.cs
+++ src/SearchService/Entities/Product.cs
@@ -9,6 +9,6 @@ public class Product : Entity
     public decimal Price { get; set; }
     public int YearOfRelease { get; set; }
     public string ImageUrl { get; set; }
-    public string CategoryName { get; set; }
-    public string BrandName { get; set; }
+    public string Category { get; set; }
+    public string Brand { get; set; }
 }
