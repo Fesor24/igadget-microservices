beea90c0a37151b546c78e7b7b7c29aac951e201
Wale <onafesowale@gmail.com>
2023-10-14 15:51:13 +0100

    product typed http client

---
+++ src/SearchService/Services/ProductHttpService.cs
@@ -0,0 +1,22 @@
+﻿using SearchService.Entities;
+
+namespace SearchService.Services;
+
+public class ProductHttpService
+{
+    private readonly HttpClient _httpClient;
+
+    public ProductHttpService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Product>> GetProducts()
+    {
+        string url = $"{_httpClient.BaseAddress}api/products";
+
+        var response = await _httpClient.GetFromJsonAsync<List<Product>>(url);
+
+        return response;
+    }
+}
