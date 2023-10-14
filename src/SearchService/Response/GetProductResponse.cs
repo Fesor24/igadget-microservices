0e32ebd46d33c17e562ad78a33f5c91e10abdab9
Wale <onafesowale@gmail.com>
2023-10-14 20:43:15 +0100

    Get product response

---
+++ src/SearchService/Response/GetProductResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace SearchService.Response;
+
+public record GetProductResponse(
+    string Id, 
+    string Name, 
+    string Description,
+    decimal Price,
+    int YearOfRelease,
+    string ImageUrl,
+    string Category,
+    string Brand);
