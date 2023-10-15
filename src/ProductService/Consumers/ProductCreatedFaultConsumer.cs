2f1df7a4d1077d9e2b21d8d2f1bb44c3865b0447
Wale <onafesowale@gmail.com>
2023-10-15 13:10:39 +0100

    product created fault consumer

---
+++ src/ProductService/Consumers/ProductCreatedFaultConsumer.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Shared.Contracts;
+
+namespace ProductService.Consumers;
+
+public class ProductCreatedFaultConsumer : IConsumer<Fault<ProductCreated>>
+{
+    private readonly ILogger<ProductCreatedFaultConsumer> _logger;
+
+    public ProductCreatedFaultConsumer(ILogger<ProductCreatedFaultConsumer> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Consume(ConsumeContext<Fault<ProductCreated>> context)
+    {
+        var exceptions = context.Message.Exceptions;
+
+        foreach (var exception in exceptions)
+        {
+            _logger.LogError($"An error occurred while consummation. " +
+                $"Message: {exception.Message} \n Details: {exception.StackTrace}");
+        }
+
+        return Task.CompletedTask;
+    }
+}
