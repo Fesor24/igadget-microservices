b057993eb019ebea5922b7dd89d0b52b396548ae
Wale <onafesowale@gmail.com>
2023-10-15 07:46:18 +0100

    Delete product functionality

---
+++ src/ProductService/Features/Product/Commands/Delete/DeleteProductCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace ProductService.Features.Product.Commands.Delete;
+
+public record DeleteProductCommand(Guid Id) : IRequest<bool>;
---
+++ src/ProductService/Features/Product/Commands/Delete/DeleteProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using ProductService.DataAccess.Contracts;
+using Shared.Exceptions;
+
+namespace ProductService.Features.Product.Commands.Delete;
+
+public sealed class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand, bool>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteProductCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _unitOfWork.ProductRepository.GetAsync(request.Id);
+
+        if (product is null)
+            throw new ApiNotFoundException($"Product with Id:{request.Id} not found");
+
+        _unitOfWork.ProductRepository.DeleteAsync(product);
+
+        var result = await _unitOfWork.Complete();
+
+        if (result < 1)
+            throw new ApiBadRequestException("No record was deleted from database");
+
+        return true;
+    }
+}
