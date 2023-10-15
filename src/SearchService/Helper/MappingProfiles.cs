32aacf02dddd1bdc45cfe282e54b018f60dd841b
Wale <onafesowale@gmail.com>
2023-10-15 07:47:16 +0100

    added mappings

---
+++ src/SearchService/Helper/MappingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using SearchService.Entities;
+using Shared.Contracts;
+
+namespace SearchService.Helper;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<ProductCreated, Product>();
+    }
+}
