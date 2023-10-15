d38a608ba63adb8c602adc39b4f93b0a889a33f1
Wale <onafesowale@gmail.com>
2023-10-15 07:47:06 +0100

    Product deleted consumer

---
+++ src/SearchService/Consumers/ProductDeletedConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using MongoDB.Entities;
+using SearchService.Entities;
+using Shared.Contracts;
+
+namespace SearchService.Consumers;
+
+public class ProductDeletedConsumer : IConsumer<ProductDeleted>
+{
+    public async Task Consume(ConsumeContext<ProductDeleted> context)
+    {
+        await DB.DeleteAsync<Product>(context.Message.Id);
+    }
+}
