c99f4866c55ce05624121eafd4460103080fae6e
Wale <onafesowale@gmail.com>
2023-10-15 07:46:54 +0100

    product created consumer

---
+++ src/SearchService/Consumers/ProductCreatedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Entities;
+using Shared.Contracts;
+
+namespace SearchService.Consumers;
+
+public class ProductCreatedConsumer : IConsumer<ProductCreated>
+{
+    private readonly IMapper _mapper;
+
+    public ProductCreatedConsumer(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public async Task Consume(ConsumeContext<ProductCreated> context)
+    {
+        var product = _mapper.Map<Product>(context.Message);
+
+        await product.SaveAsync();
+    }
+}
