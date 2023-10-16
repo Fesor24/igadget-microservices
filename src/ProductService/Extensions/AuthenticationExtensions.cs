79fd11a95b88597c9d7cc3a21f9dbf98172d5764
Wale <onafesowale@gmail.com>
2023-10-16 08:05:46 +0100

    Authentication extensions

---
+++ src/ProductService/Extensions/AuthenticationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ProductService.Extensions;
+
+public static class AuthenticationExtensions
+{
+    public static void AddApplicationAuthentication(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt =>
+            {
+                opt.Authority = config["IdentityServerUrl"];
+                opt.Audience = "productapi";
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidTypes = new[] {"at+jwt"}
+                };
+                opt.RequireHttpsMetadata = false;
+            });
+
+        services.AddAuthorization();
+    }
+}
