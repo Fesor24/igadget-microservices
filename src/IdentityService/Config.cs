1ef49d738edc0d6628158673719c688a02a31072
Wale <onafesowale@gmail.com>
2023-10-16 08:10:57 +0100

    config

---
+++ src/IdentityService/Config.cs
@@ -0,0 +1,60 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityService;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+    public static IEnumerable<ApiResource> ApiResources =>
+        new ApiResource[]
+        {
+            new ApiResource("productapi", "Product Api")
+            {
+                Scopes = new []{"productapi.read", "productapi.write"}
+            }
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("productapi.read", "Product Api Read"),
+            new ApiScope("productapi.write", "Product Api Write"),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            // m2m client credentials flow client
+            new Client
+            {
+                ClientId = "search_service",
+                ClientName = "Search Service",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("search-secret".Sha256()) },
+
+                AllowedScopes = { "productapi.read", "productapi.write" },
+            }
+
+            // interactive client using code flow + pkce
+            //new Client
+            //{
+            //    ClientId = "interactive",
+            //    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+
+            //    AllowedGrantTypes = GrantTypes.Code,
+
+            //    RedirectUris = { "https://localhost:44300/signin-oidc" },
+            //    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
+            //    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+
+            //    AllowOfflineAccess = true,
+            //    AllowedScopes = { "openid", "profile", "scope2" }
+            //},
+        };
+}
