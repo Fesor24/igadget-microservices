08b367904b9677a801b588e511c241738cd3b7a9
Wale <onafesowale@gmail.com>
2023-10-16 08:08:34 +0100

    Added authentication to DI container and middleware pipeline

--- src/ProductService/Program.cs
+++ src/ProductService/Program.cs
@@ -4,9 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationServices(builder.Configuration);
 
+builder.Services.AddApplicationAuthentication(builder.Configuration);
+
 var app = builder.Build();
 
 app.ConfigureExceptionHandler();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.RegisterEndpoints();
 
