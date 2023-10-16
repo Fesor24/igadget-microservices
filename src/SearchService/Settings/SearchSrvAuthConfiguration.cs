8dd32d3fc643e3c6a2614290cfeb6dd45dc6f6bc
Wale <onafesowale@gmail.com>
2023-10-16 08:07:54 +0100

    Search service auth configuration

---
+++ src/SearchService/Settings/SearchSrvAuthConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace SearchService.Settings;
+
+public class SearchSrvAuthConfiguration
+{
+    public const string CONFIGURATION = "SearchSrvAuthConfiguration";
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    public string GrantTypes { get; set; }
+    public string Scope { get; set; }
+}
