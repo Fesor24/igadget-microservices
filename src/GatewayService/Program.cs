b3b77404f9dc88a96108f4335b39917b7232ba4f
Wale <onafesowale@gmail.com>
2023-10-17 09:32:56 +0100

    added services to service collection

---
+++ src/GatewayService/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServerUrl"];
+        options.RequireHttpsMetadata = false;
+        options.Audience = "productapi";
+    });
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.Run();
