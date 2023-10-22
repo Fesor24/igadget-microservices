48b99f4aaa17ae89196222f09ff9689445bae925
Wale <onafesowale@gmail.com>
2023-10-22 13:37:47 +0100

    commenting out code

--- src/SearchService/Services/ProductHttpService.cs
+++ src/SearchService/Services/ProductHttpService.cs
@@ -23,38 +23,41 @@ public class ProductHttpService
     {
         string prdUrl = $"{_httpClient.BaseAddress}api/products";
 
+        // Commented code was when I had the product service authenticated
+        // Authentication has been removed so there is no need for this atm
+
         // Send request to id server to get token
-        var formContent = new List<KeyValuePair<string, string>>
-        {
-            new KeyValuePair<string, string>("client_id", _authConfig.ClientId),
-            new KeyValuePair<string, string>("client_secret", _authConfig.ClientSecret),
-            new KeyValuePair<string, string>("grant_type", _authConfig.GrantTypes),
-            new KeyValuePair<string, string>("scope", _authConfig.Scope)
-        };
+        //var formContent = new List<KeyValuePair<string, string>>
+        //{
+        //    new KeyValuePair<string, string>("client_id", _authConfig.ClientId),
+        //    new KeyValuePair<string, string>("client_secret", _authConfig.ClientSecret),
+        //    new KeyValuePair<string, string>("grant_type", _authConfig.GrantTypes),
+        //    new KeyValuePair<string, string>("scope", _authConfig.Scope)
+        //};
 
-        var content = new FormUrlEncodedContent(formContent);
+        //var content = new FormUrlEncodedContent(formContent);
 
-        var idSrvRes = await _httpClient.PostAsync($"{ _config["IdentityServerUrl"]}/connect/token", content);
+        //var idSrvRes = await _httpClient.PostAsync($"{ _config["IdentityServerUrl"]}/connect/token", content);
 
-        IdSrvTokenResponse tokenResponse = default;
+        //IdSrvTokenResponse tokenResponse = default;
 
-        if (idSrvRes.IsSuccessStatusCode)
-        {
-            tokenResponse = await idSrvRes.Content.ReadFromJsonAsync<IdSrvTokenResponse>();
-        }
-        else
-        {
-            var error = await idSrvRes.Content.ReadAsStreamAsync();
+        //if (idSrvRes.IsSuccessStatusCode)
+        //{
+        //    tokenResponse = await idSrvRes.Content.ReadFromJsonAsync<IdSrvTokenResponse>();
+        //}
+        //else
+        //{
+        //    var error = await idSrvRes.Content.ReadAsStreamAsync();
 
-            var errorMessage = idSrvRes.ReasonPhrase;
+        //    var errorMessage = idSrvRes.ReasonPhrase;
 
-            Console.WriteLine(new { error, errorMessage});
-        }
+        //    Console.WriteLine(new { error, errorMessage});
+        //}
 
-        if (tokenResponse is null || string.IsNullOrWhiteSpace(tokenResponse.AccessToken))
-            throw new ApiNotAuthorizedException("Token not present yet");
+        //if (tokenResponse is null || string.IsNullOrWhiteSpace(tokenResponse.AccessToken))
+        //    throw new ApiNotAuthorizedException("Token not present yet");
 
-        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
+        //_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
 
         var response = await _httpClient.GetFromJsonAsync<List<Product>>(prdUrl);
 
