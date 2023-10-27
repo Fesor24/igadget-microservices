3dc6e7235f8f53ff775b00c405ae3f129acd9ebd
Wale <onafesowale@gmail.com>
2023-10-27 19:14:11 +0100

    IShoppingCartRepository

---
+++ src/ShoppingCartService/Repositories/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShoppingCartService.Entities;
+
+namespace ShoppingCartService.Repositories.Contracts;
+
+public interface IShoppingCartRepository
+{
+    Task<ShoppingCart> GetCartAsync(string cartId);
+
+    Task<ShoppingCart> AddEditCartAsync(ShoppingCart cart);
+
+    Task<bool> DeleteCartAsync(string cartId);
+}
