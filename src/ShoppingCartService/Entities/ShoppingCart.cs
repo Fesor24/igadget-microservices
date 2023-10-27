af461e2314d6a558fe638896a9508e1cecc6c0c3
Wale <onafesowale@gmail.com>
2023-10-27 19:12:50 +0100

    entities

---
+++ src/ShoppingCartService/Entities/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿namespace ShoppingCartService.Entities;
+
+public class ShoppingCart
+{
+    public ShoppingCart()
+    {
+        
+    }
+
+    public ShoppingCart(string id)
+    {
+        Id = id;
+    }
+
+    public string Id { get; set; }
+    public List<ShoppingCartItems> Items { get; set; } = new();
+}
---
+++ src/ShoppingCartService/Entities/ShoppingCartItems.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoppingCartService.Entities;
+
+public class ShoppingCartItems
+{
+    public string Id { get; set; }
+    public string Product { get; set; } 
+    public decimal Price { get; set; }  
+    public int Quantity { get; set; }
+    public string ImageUrl { get; set; }
+    public string Brand { get; set; }
+    public string Category { get; set; }
+}
