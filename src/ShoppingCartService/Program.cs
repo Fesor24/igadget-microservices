ac756ab8fb6e19785c28cd913c6afea1d46689cc
Wale <onafesowale@gmail.com>
2023-10-27 19:13:23 +0100

    add connection multiplexer to service collection

--- src/ShoppingCartService/Program.cs
+++ src/ShoppingCartService/Program.cs
@@ -1,4 +1,22 @@
+using ShoppingCartService.EndpointDefinitions;
+using ShoppingCartService.Repositories.Contracts;
+using ShoppingCartService.Repositories.Implementation;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(opt =>
+{
+    var configurationOptions = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis")
+        ,true);
+
+    return ConnectionMultiplexer.Connect(configurationOptions);
+});
+
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+
 var app = builder.Build();
+
+ShoppingCartEndpointDefinition.RegisterEndpoint(app);
 
 app.Run();
