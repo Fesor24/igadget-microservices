206a3154ed02b235be8d1e01343ab60ef860ec90
Wale <onafesowale@gmail.com>
2023-10-27 19:14:21 +0100

    ShoppingCartRepository

---
+++ src/ShoppingCartService/Repositories/Implementation/ShoppingCartRepository.cs
@@ -0,0 +1,36 @@
+﻿using ShoppingCartService.Entities;
+using ShoppingCartService.Repositories.Contracts;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace ShoppingCartService.Repositories.Implementation;
+
+public class ShoppingCartRepository : IShoppingCartRepository
+{
+    private readonly IDatabase _db;
+
+    public ShoppingCartRepository(IConnectionMultiplexer connectionMultiplexer)
+    {
+        _db = connectionMultiplexer.GetDatabase();
+    }
+
+    public async Task<ShoppingCart> AddEditCartAsync(ShoppingCart cart)
+    {
+        var serCart = JsonSerializer.Serialize(cart);
+
+        var result = await _db.StringSetAsync(cart.Id, serCart, expiry: TimeSpan.FromDays(30));
+
+        return result ? await GetCartAsync(cart.Id) : null; 
+    }
+
+    public async Task<bool> DeleteCartAsync(string cartId) => 
+        await _db.KeyDeleteAsync(cartId);
+
+    public async Task<ShoppingCart> GetCartAsync(string cartId)
+    {
+        var cart = await _db.StringGetAsync(cartId);
+
+        return cart.IsNullOrEmpty ? null : 
+            JsonSerializer.Deserialize<ShoppingCart>(cart);
+    }
+}
