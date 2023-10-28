c8350d1bf28f7812d607ad248d65cc0c31253e17
Wale <onafesowale@gmail.com>
2023-10-28 05:03:07 +0100

    migration files

---
+++ src/IdentityService/Data/Migrations/20231028014836__added_props.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _added_props : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNo",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNo",
+                table: "AspNetUsers");
+        }
+    }
+}
