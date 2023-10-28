276fabc526c4c8fbb01c61766081f7528f91d64f
Wale <onafesowale@gmail.com>
2023-10-28 05:05:52 +0100

    Application extensions

---
+++ src/OrderService/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+
+namespace OrderService.Extensions;
+
+public static class ApplicationExtensions
+{
+    public static void RegisterServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        services.AddDbContext<OrderDbContext>(options =>
+        {
+            options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+        });
+    }
+}
