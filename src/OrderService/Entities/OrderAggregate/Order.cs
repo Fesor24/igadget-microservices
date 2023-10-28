b7af3590a40c6dc8c0bcba299f39ca3397bd37ba
Wale <onafesowale@gmail.com>
2023-10-28 05:04:44 +0100

    order entity

---
+++ src/OrderService/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,18 @@
+﻿using OrderService.Enums;
+
+namespace OrderService.Entities.OrderAggregate;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public Address DeliveryAddress { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+    public decimal SubTotal { get; set; }
+    public DeliveryMethod DeliveryMethod { get; set; }
+    public string BuyerEmail { get; set; }
+    public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+    public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Pending;
+    public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+    public decimal GetTotal() =>
+        SubTotal + DeliveryMethod.Price;
+}
