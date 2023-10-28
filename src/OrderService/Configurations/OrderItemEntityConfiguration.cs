e2b63161927c6c6a6f5bab136ff112407830a169
Wale <onafesowale@gmail.com>
2023-10-28 05:03:51 +0100

    order item entity config

---
+++ src/OrderService/Configurations/OrderItemEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderService.Entities.OrderAggregate;
+
+namespace OrderService.Configurations;
+
+public class OrderItemEntityConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.ToTable(nameof(OrderItem), "ord");
+        builder.HasKey(x => x.Id);
+        builder.OwnsOne(x => x.ItemOrdered, c =>
+        {
+            c.Property(x => x.ProductName).IsRequired();
+            c.Property(x => x.ProductId).IsRequired();
+            c.WithOwner();
+        });
+    }
+}
