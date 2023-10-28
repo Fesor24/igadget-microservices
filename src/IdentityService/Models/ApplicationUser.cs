8d18fac79ebed862acfaa79fb744527a45eaf5f9
Wale <onafesowale@gmail.com>
2023-10-28 05:03:19 +0100

    Added props to application user

--- src/IdentityService/Models/ApplicationUser.cs
+++ src/IdentityService/Models/ApplicationUser.cs
@@ -9,4 +9,6 @@ namespace IdentityService.Models;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    public string PhoneNo { get; set; }
+    public string DisplayName { get; set; }
 }
