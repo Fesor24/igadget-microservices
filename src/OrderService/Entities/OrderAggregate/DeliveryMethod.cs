555ad252fa97dbb5b3314b6e40710aca71343316
Wale <onafesowale@gmail.com>
2023-10-28 05:04:34 +0100

    delivery method entity

---
+++ src/OrderService/Entities/OrderAggregate/DeliveryMethod.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderService.Entities.OrderAggregate;
+
+public class DeliveryMethod
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string DeliveryTime { get; set; }
+    public decimal Price { get; set; }
+}
