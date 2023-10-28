07bcb150a39fc2ba301834ceb67d1b2de7b5baa7
Wale <onafesowale@gmail.com>
2023-10-28 05:04:52 +0100

    order item entity

---
+++ src/OrderService/Entities/OrderAggregate/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderService.Entities.OrderAggregate;
+
+public class OrderItem
+{
+    public Guid Id { get; set; }
+    public ProductItemOrdered ItemOrdered { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
