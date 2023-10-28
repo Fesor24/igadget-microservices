4c94a4596fb243114df600ae0f3e42e89785bae8
Wale <onafesowale@gmail.com>
2023-10-28 05:04:00 +0100

    product entity config

---
+++ src/OrderService/Configurations/ProductEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderService.Entities;
+
+namespace OrderService.Configurations;
+
+public class ProductEntityConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Product", "ord");
+        builder.HasKey(x => x.Id);
+    }
+}
