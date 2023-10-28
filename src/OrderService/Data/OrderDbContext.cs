3abb17b48ad05afe3cbc370164d1bea317b6f41b
Wale <onafesowale@gmail.com>
2023-10-28 05:04:15 +0100

    order db context

---
+++ src/OrderService/Data/OrderDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Entities;
+using OrderService.Entities.OrderAggregate;
+using System.Reflection;
+
+namespace OrderService.Data;
+
+public class OrderDbContext : DbContext
+{
+    public OrderDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Order> Order => Set<Order>();
+
+    public DbSet<Product> Product => Set<Product>();
+    public DbSet<DeliveryMethod> DeliveryMethod => Set<DeliveryMethod>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
