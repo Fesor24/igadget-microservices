112365fa044c7925d708c29542355314d1eb5ed6
Wale <onafesowale@gmail.com>
2023-10-28 05:03:32 +0100

    delivery method entity config

---
+++ src/OrderService/Configurations/DeliveryMethodEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderService.Entities.OrderAggregate;
+
+namespace OrderService.Configurations;
+
+public class DeliveryMethodEntityConfiguration : IEntityTypeConfiguration<DeliveryMethod>
+{
+    public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+    {
+        builder.ToTable(nameof(DeliveryMethod), "ord");
+        builder.HasKey(x => x.Id);
+    }
+}
