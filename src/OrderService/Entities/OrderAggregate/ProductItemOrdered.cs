01c2d12924b9f9547b0fc34a6039f8cf0bcac11e
Wale <onafesowale@gmail.com>
2023-10-28 05:05:04 +0100

    product item ordered value entity

---
+++ src/OrderService/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderService.Entities.OrderAggregate;
+
+public class ProductItemOrdered
+{
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; }
+    public string ImageUrl { get; set; }
+}
