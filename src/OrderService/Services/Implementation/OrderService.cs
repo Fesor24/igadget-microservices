6fbd3cce6f3b761ddc918469db342cfed3505500
Wale <onafesowale@gmail.com>
2023-10-29 19:16:10 +0100

    order service

---
+++ src/OrderService/Services/Implementation/OrderService.cs
@@ -0,0 +1,12 @@
+﻿using OrderService.Requests;
+using OrderService.Services.Contracts;
+
+namespace OrderService.Services.Implementation;
+
+public class OrderService : IOrderService
+{
+    public Task CreateOrderAsync(CreateOrderRequest orderRequest)
+    {
+        throw new NotImplementedException();
+    }
+}
