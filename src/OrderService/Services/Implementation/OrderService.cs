5af77e337f4626598306b643edc9feaa60550a1c
Wale <onafesowale@gmail.com>
2023-10-31 09:41:50 +0100

    Added methods to service

--- src/OrderService/Services/Implementation/OrderService.cs
+++ src/OrderService/Services/Implementation/OrderService.cs
@@ -1,12 +1,31 @@
-﻿using OrderService.Requests;
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+using OrderService.Entities.OrderAggregate;
+using OrderService.Requests;
 using OrderService.Services.Contracts;
 
 namespace OrderService.Services.Implementation;
 
 public class OrderService : IOrderService
 {
-    public Task CreateOrderAsync(CreateOrderRequest orderRequest)
+    private readonly OrderDbContext _context;
+
+    public OrderService(OrderDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<Order> CreateOrderAsync(CreateOrderRequest orderRequest)
     {
         throw new NotImplementedException();
     }
+
+    public async Task<Order> GetOrderByIdAsync(string id) =>
+        await _context.Order
+        .FirstOrDefaultAsync(x => x.Id == Guid.Parse(id));
+
+    public async Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail) =>
+        await _context.Order
+        .Where(x => x.BuyerEmail == buyerEmail)
+        .ToListAsync();
 }
