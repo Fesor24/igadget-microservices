afcdc499341dafd3f9c303aa7654345ccd1f2a49
Wale <onafesowale@gmail.com>
2023-10-31 09:43:26 +0100

    Grpc cart svc

---
+++ src/ShoppingCartService/Services/GrpcCartService.cs
@@ -0,0 +1,48 @@
+﻿using Google.Protobuf.Collections;
+using Grpc.Core;
+using ShoppingCartService.Repositories.Contracts;
+
+namespace ShoppingCartService.Services;
+
+public class GrpcCartService : GrpcCart.GrpcCartBase
+{
+    private readonly IShoppingCartRepository _shoppingCart;
+
+    public GrpcCartService(IShoppingCartRepository shoppingCart)
+    {
+        _shoppingCart = shoppingCart;
+    }
+
+    public override async Task<GrpcCartResponse> GetCart(GetCartRequest request, ServerCallContext context)
+    {
+        var cart = await _shoppingCart.GetCartAsync(request.Id) ?? 
+            throw new RpcException(new Status(StatusCode.NotFound, $"Cart with Id: {request.Id} not found"));
+
+        var cartItems = new RepeatedField<GrpcCartItemsModel>();
+
+        foreach(var item in cart.Items)
+        {
+            var cartItem = new GrpcCartItemsModel
+            {
+                ImageUrl = item.ImageUrl,
+                ProductId = item.ProductId,
+                Quantity = item.Quantity,
+                ProductName = item.ProductName
+            };
+
+            cartItems.Add(cartItem);
+        }
+
+        var response = new GrpcCartResponse
+        {
+            Cart = new GrpcCartModel
+            {
+                Id = cart.Id
+            }
+        };
+
+        response.Cart.CartItems.Add(cartItems);
+
+        return response;
+    }
+}
