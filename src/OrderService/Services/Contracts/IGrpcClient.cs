a0373ff536f53c9d043564b828accb076486b5b3
Wale <onafesowale@gmail.com>
2023-11-01 15:54:22 +0100

    IGrpc client

---
+++ src/OrderService/Services/Contracts/IGrpcClient.cs
@@ -0,0 +1,10 @@
+﻿using OrderService.Entities;
+using OrderService.Models;
+
+namespace OrderService.Services.Contracts;
+
+public interface IGrpcClient
+{
+    ShoppingCart GetShoppingCart(string id);
+    Product GetProduct(string id);
+}
