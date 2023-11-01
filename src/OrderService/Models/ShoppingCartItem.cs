6785997c34c36779170f33bbfe367251ce381998
Wale <onafesowale@gmail.com>
2023-11-01 15:54:04 +0100

    shopping cart item model

---
+++ src/OrderService/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderService.Models;
+
+public class ShoppingCartItem
+{
+    public string ProductId { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string ImageUrl { get; set; }
+    public string Brand { get; set; }
+    public string Category { get; set; }
+}
