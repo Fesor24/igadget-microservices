a87b5cf83d74a4fb2860aba70bbb5447ba9b557f
Wale <onafesowale@gmail.com>
2023-11-01 15:53:22 +0100

    Order endpoint definition

---
+++ src/OrderService/Endpoints/OrderEndpointDefinition.cs
@@ -0,0 +1,19 @@
+﻿using OrderService.Services.Contracts;
+
+namespace OrderService.Endpoints;
+
+public class OrderEndpointDefinition
+{
+    public static void RegisterEndpoints(WebApplication app)
+    {
+        var order = app.MapGroup("api/order");
+
+        // Endpoint to test the grpc cart client
+        order.MapGet("/grpc-test/{id}", (string id, IGrpcClient cartClient) =>
+        {
+            var cart = cartClient.GetShoppingCart(id);
+
+            return Results.Ok(cart);
+        });
+    }
+}
