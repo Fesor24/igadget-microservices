7f5cfc5568e5e87f08e052dbc0a1b04d460fa61a
Wale <onafesowale@gmail.com>
2023-11-03 09:18:37 +0100

    custom profile service to include user claims in access token

---
+++ src/IdentityService/Services/CustomProfileService.cs
@@ -0,0 +1,42 @@
+﻿using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace IdentityService.Services;
+
+public class CustomProfileService : IProfileService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public CustomProfileService(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+    {
+        var user = await _userManager.GetUserAsync(context.Subject);
+
+        if(user is not null)
+        {
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            var userClaims = new List<Claim>
+            {
+                new Claim("username", user.UserName),
+                new Claim(JwtClaimTypes.Email, user.Email)
+            };
+
+            context.IssuedClaims.AddRange(userClaims);
+        }
+
+    }
+
+    public Task IsActiveAsync(IsActiveContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
