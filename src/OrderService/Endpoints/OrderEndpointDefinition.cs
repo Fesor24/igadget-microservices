3cad9355daaa595670b56789b867ef22c9c85880
Wale <onafesowale@gmail.com>
2023-11-03 09:19:30 +0100

    added product grpc test endpoint

--- src/OrderService/Endpoints/OrderEndpointDefinition.cs
+++ src/OrderService/Endpoints/OrderEndpointDefinition.cs
@@ -9,11 +9,20 @@ public class OrderEndpointDefinition
         var order = app.MapGroup("api/order");
 
         // Endpoint to test the grpc cart client
-        order.MapGet("/grpc-test/{id}", (string id, IGrpcClient cartClient) =>
+        order.MapGet("/grpc-cart-test/{id}", (string id, IGrpcClient cartClient) =>
         {
             var cart = cartClient.GetShoppingCart(id);
 
             return Results.Ok(cart);
         });
+
+        // Endpoint to test the grpc product client
+        order.MapGet("/grpc-prd-test/{id}", (string id, IGrpcClient cartClient) =>
+        {
+            var product = cartClient.GetProduct(id);
+
+            return Results.Ok(product);
+        })
+            .RequireAuthorization();
     }
 }
