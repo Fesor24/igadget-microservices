342c38beffd5c15e3aa4e4273fcec3a358b4f9be
Wale <onafesowale@gmail.com>
2023-11-04 12:21:44 +0100

    Endpoint extensions

---
+++ src/OrderService/Extensions/EndpointExtensions.cs
@@ -0,0 +1,22 @@
+﻿using OrderService.Endpoints;
+using System.Reflection;
+
+namespace OrderService.Extensions;
+
+public static class EndpointExtensions
+{
+    public static void RegisterApplicationEndpoints(this WebApplication app)
+    {
+        var endpoints = Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .Where(x => typeof(IEndpointDefinition).IsAssignableFrom(x) && !x.IsAbstract && !x.IsInterface)
+            .Select(Activator.CreateInstance)
+            .Cast<IEndpointDefinition>();
+
+        foreach(var endpoint in endpoints)
+        {
+            endpoint.RegisterEndpoints(app);
+        }
+
+    }
+}
