d038c02141efef8e4d4dc3326d4cd534ed043e36
Wale <onafesowale@gmail.com>
2023-11-04 12:26:24 +0100

    get order response

---
+++ src/OrderService/Response/GetOrderResponse.cs
@@ -0,0 +1,14 @@
+﻿using OrderService.Enums;
+
+namespace OrderService.Response;
+
+public class GetOrderResponse
+{
+    public string OrderId { get; set; }
+    public List<OrderItemsResponse> OrderItems { get; set; }
+    public string OrderStatus { get; set; }
+    public string PaymentStatus { get; set; }
+    public decimal SubTotal { get; set; }
+    public decimal Total { get; set; }  
+    public decimal DeliveryCharges { get; set; }
+}
