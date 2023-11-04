8e5690eb5deacd68352882e83276b5e5f088241f
Wale <onafesowale@gmail.com>
2023-11-04 12:26:47 +0100

    Delivery method service

---
+++ src/OrderService/Services/Contracts/IDeliveryMethodService.cs
@@ -0,0 +1,11 @@
+﻿using OrderService.Requests;
+using OrderService.Response;
+
+namespace OrderService.Services.Contracts;
+
+public interface IDeliveryMethodService
+{
+    Task<GetDeliveryMethodResponse> CreateDeliveryMethod(CreateDeliveryMethodRequest request);
+    Task<IReadOnlyList<GetDeliveryMethodResponse>> GetDeliveryMethods();
+    Task<GetDeliveryMethodResponse> GetDeliveryMethod(Guid  deliveryMethodId);
+}
---
+++ src/OrderService/Services/Implementation/DeliveryMethodService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+using OrderService.Entities.OrderAggregate;
+using OrderService.Requests;
+using OrderService.Response;
+using OrderService.Services.Contracts;
+using Shared.Exceptions;
+
+namespace OrderService.Services.Implementation;
+
+public class DeliveryMethodService : IDeliveryMethodService
+{
+    private readonly OrderDbContext _context;
+    private readonly IMapper _mapper;
+
+    public DeliveryMethodService(OrderDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<GetDeliveryMethodResponse> CreateDeliveryMethod(CreateDeliveryMethodRequest request)
+    {
+        var deliveryMethod = _mapper.Map<DeliveryMethod>(request);
+
+        await _context.DeliveryMethod.AddAsync(deliveryMethod);
+
+        await _context.SaveChangesAsync();
+
+        return _mapper.Map<GetDeliveryMethodResponse>(deliveryMethod);
+    }
+
+    public async Task<GetDeliveryMethodResponse> GetDeliveryMethod(Guid deliveryMethodId)
+    {
+        var deliveryMethod = await _context.DeliveryMethod
+            .FirstOrDefaultAsync(x => x.Id == deliveryMethodId) ??
+            throw new ApiNotFoundException($"Delivery method with id: {deliveryMethodId} not found");
+
+        return _mapper.Map<GetDeliveryMethodResponse>(deliveryMethod);
+    }
+
+    public async Task<IReadOnlyList<GetDeliveryMethodResponse>> GetDeliveryMethods()
+    {
+        var deliveryMethods = await _context.DeliveryMethod.ToListAsync();
+
+        return _mapper.Map<IReadOnlyList<GetDeliveryMethodResponse>>(deliveryMethods);
+    }
+}
