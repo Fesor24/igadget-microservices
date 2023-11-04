4490fd18f5c05f7db82aaa6aab27f36fe3c93cd6
Wale <onafesowale@gmail.com>
2023-11-04 12:20:51 +0100

    added enum member attr

--- src/OrderService/Enums/PaymentStatus.cs
+++ src/OrderService/Enums/PaymentStatus.cs
@@ -1,8 +1,13 @@
-﻿namespace OrderService.Enums;
+﻿using System.Runtime.Serialization;
+
+namespace OrderService.Enums;
 
 public enum PaymentStatus
 {
+    [EnumMember(Value = "Pending")]
     Pending,
+    [EnumMember(Value = "Successful")]
     Successful,
+    [EnumMember(Value = "Failed")]
     Failed
 }
