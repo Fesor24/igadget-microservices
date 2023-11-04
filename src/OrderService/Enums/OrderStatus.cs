d491ebe359f4aa03f29460424fa6504d91e95f51
Wale <onafesowale@gmail.com>
2023-11-04 12:20:43 +0100

    added enum member attr

--- src/OrderService/Enums/OrderStatus.cs
+++ src/OrderService/Enums/OrderStatus.cs
@@ -1,9 +1,15 @@
-﻿namespace OrderService.Enums;
+﻿using System.Runtime.Serialization;
+
+namespace OrderService.Enums;
 
 public enum OrderStatus
 {
+    [EnumMember(Value = "Pending")]
     Pending,
+    [EnumMember(Value = "Shipped")]
     Shipped,
+    [EnumMember(Value = "Cancelled")]
     Cancelled,
+    [EnumMember(Value = "Delivered")]
     Delivered
 }
