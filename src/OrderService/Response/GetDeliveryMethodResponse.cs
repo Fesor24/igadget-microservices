94fd67d330d2842d856f821e24f51f889aed2346
Wale <onafesowale@gmail.com>
2023-11-04 12:26:13 +0100

    get delivery method response

---
+++ src/OrderService/Response/GetDeliveryMethodResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderService.Response;
+
+public class GetDeliveryMethodResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string DeliveryTime { get; set; }
+    public decimal Price { get; set; }
+}
