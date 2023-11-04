4778fb7c139fc21280b77d759f789452dbd06b37
Wale <onafesowale@gmail.com>
2023-11-04 12:25:33 +0100

    create delivery method request

---
+++ src/OrderService/Requests/CreateDeliveryMethodRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace OrderService.Requests;
+
+public class CreateDeliveryMethodRequest
+{
+    public string Name { get; set; }
+    public string DeliveryTime { get; set; }
+    public decimal Price { get; set; }
+}
