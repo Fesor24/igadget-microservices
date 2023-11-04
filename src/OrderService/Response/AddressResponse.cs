0d174b57c5beb9ee5738bcb2baa9a5860104d340
Wale <onafesowale@gmail.com>
2023-11-04 12:25:52 +0100

    address response

---
+++ src/OrderService/Response/AddressResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderService.Response;
+
+public class AddressResponse
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+}
