1ebfc4d9648c0d3fd8a0f031f360dd850015ec0f
Wale <onafesowale@gmail.com>
2023-11-04 12:26:33 +0100

    order items response

---
+++ src/OrderService/Response/OrderItemsResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderService.Response;
+
+public class OrderItemsResponse
+{
+    public string OrderItemId { get; set; }
+    public string ProductName { get; set; }
+    public string ImageUrl { get; set; }
+    public string Category { get; set; }
+    public string Brand { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
