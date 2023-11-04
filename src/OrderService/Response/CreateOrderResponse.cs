db76e3cc6d409380765528dcde2868f63cdf4c9f
Wale <onafesowale@gmail.com>
2023-11-04 12:26:03 +0100

    create order response

---
+++ src/OrderService/Response/CreateOrderResponse.cs
@@ -0,0 +1,15 @@
+﻿using OrderService.Enums;
+
+namespace OrderService.Response;
+
+public class CreateOrderResponse
+{
+    public string OrderId { get; set; }
+    public AddressResponse DeliveryAddress { get; set; }
+    public List<OrderItemsResponse> OrderItems { get; set; }
+    public decimal Subtotal { get; set; }
+    public decimal Total { get; set; }
+    public OrderStatus OrderStatus { get; set; }
+    public PaymentStatus PaymentStatus { get; set; }
+    public DateTimeOffset OrderDate { get; set; }
+}
