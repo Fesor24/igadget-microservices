be054b267e3799024cb697475db26760dc91b6e2
Wale <onafesowale@gmail.com>
2023-11-04 12:25:43 +0100

    create order request

--- src/OrderService/Requests/CreateOrderRequest.cs
+++ src/OrderService/Requests/CreateOrderRequest.cs
@@ -2,7 +2,6 @@
 
 public class CreateOrderRequest
 {
-    public string BuyerEmail { get; set; }
     public string DeliveryMethodId { get; set; }
     public AddressRequest DeliveryAddress { get; set; }
     public string CartId { get; set; }
