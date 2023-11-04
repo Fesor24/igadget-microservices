24783d016ad5850fbd87985834d2a65d8cec0f3b
Wale <onafesowale@gmail.com>
2023-11-04 12:21:08 +0100

    mapping profiles

---
+++ src/OrderService/Helpers/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using OrderService.Entities.OrderAggregate;
+using OrderService.Requests;
+using OrderService.Response;
+
+namespace OrderService.Helpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<CreateDeliveryMethodRequest, DeliveryMethod>();
+        CreateMap<DeliveryMethod, GetDeliveryMethodResponse>();
+    }
+}
