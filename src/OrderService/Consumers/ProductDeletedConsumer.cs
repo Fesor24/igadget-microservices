68daf1688d6150ccf58c2b3a85ca69fa54947797
Wale <onafesowale@gmail.com>
2023-11-12 15:24:09 +0100

    prd deleted consumer

---
+++ src/OrderService/Consumers/ProductDeletedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+using Shared.Contracts;
+
+namespace OrderService.Consumers;
+
+public class ProductDeletedConsumer : IConsumer<ProductDeleted>
+{
+    private readonly OrderDbContext _context;
+
+    public ProductDeletedConsumer(OrderDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Consume(ConsumeContext<ProductDeleted> context)
+    {
+        var product = await _context.Product
+            .FirstOrDefaultAsync(x => x.Id == Guid.Parse(context.Message.Id));
+
+        if (product is null)
+            return;
+
+        _context.Product.Remove(product);
+
+        await _context.SaveChangesAsync();
+    }
+}
