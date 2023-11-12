3c90fda4f94309866c5e0dbada236979e627ce89
Wale <onafesowale@gmail.com>
2023-11-12 15:24:35 +0100

    modified return type

--- src/ProductService/DataAccess/Repository/ProductRepository.cs
+++ src/ProductService/DataAccess/Repository/ProductRepository.cs
@@ -2,6 +2,8 @@
 using ProductService.Data;
 using ProductService.DataAccess.Contracts;
 using ProductService.Entities;
+using ProductService.Models;
+using ProductService.Response;
 
 namespace ProductService.DataAccess.Repository;
 
@@ -14,15 +16,37 @@ public class ProductRepository : GenericRepository<Product>, IProductRepository
         _context = context;
     }
 
-    public async Task<Product> GetProductDetails(Guid id) =>
+    public async Task<ProductModel> GetProductDetails(Guid id) =>
         await _context.Products.Where(x => x.Id == id)
             .Include(x => x.Category)
             .Include(x => x.Brand)
+            .Select(x => new ProductModel
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Price = x.Price,
+                Brand = x.Brand.Name,
+                Category = x.Category.Name,
+                Id = x.Id.ToString(),
+                ImageUrl = x.ImageUrl,
+                YearOfRelease = x.YearOfRelease
+            })
             .FirstOrDefaultAsync();
 
-    public async Task<IReadOnlyList<Product>> GetProductsDetails() =>
+    public async Task<IReadOnlyList<ProductModel>> GetProductsDetails() =>
         await _context.Products
             .Include(x => x.Category)
             .Include(x => x.Brand)
+            .Select(x => new ProductModel
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Price = x.Price,
+                Brand = x.Brand.Name,
+                Category = x.Category.Name,
+                Id = x.Id.ToString(),
+                ImageUrl = x.ImageUrl,
+                YearOfRelease = x.YearOfRelease
+            })
             .ToListAsync();
 }
