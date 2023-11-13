de2f2ae61a27b2e4a77b8aa6c05fa32631a34a0a
Wale <onafesowale@gmail.com>
2023-11-13 16:18:41 +0100

    Product svc application factory

---
+++ tests/ProductService.IntegrationTests/Fixtures/ProductSvcApplicationFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ProductService.IntegrationTests.Fixtures;
+public class ProductSvcApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    public Task InitializeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    Task IAsyncLifetime.DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
