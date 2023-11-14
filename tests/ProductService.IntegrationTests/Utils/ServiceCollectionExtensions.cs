8ff0731e7b6ba32460ecaff2d60db29f14af16ea
Wale <onafesowale@gmail.com>
2023-11-14 07:11:08 +0100

    service collection extensions

---
+++ tests/ProductService.IntegrationTests/Utils/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProductService.Data;
+
+namespace ProductService.IntegrationTests.Utils;
+public static class ServiceCollectionExtensions
+{
+    public static void RemoveDbContext<T>(this IServiceCollection services) where T : class
+    {
+        var dbContextDescriptor = services.SingleOrDefault(d =>
+           d.ServiceType == typeof(DbContextOptions<ProductDbContext>));
+
+        if (dbContextDescriptor != null) services.Remove(dbContextDescriptor);
+    }
+
+    public static void EnsureCreated<T>(this IServiceCollection services) where T: class
+    {
+        var sp = services.BuildServiceProvider();
+
+        using var scope = sp.CreateScope();
+
+        var db = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+
+        db.Database.Migrate();
+
+        Database.InitializeDb(db);
+    }
+}
