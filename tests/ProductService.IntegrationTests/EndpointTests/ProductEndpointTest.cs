c730ce528dd561f1ff3542223eaf48ff5303c1b4
Wale <onafesowale@gmail.com>
2023-11-14 07:10:51 +0100

    Product GET endpoint tests

---
+++ tests/ProductService.IntegrationTests/EndpointTests/ProductEndpointTest.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.Extensions.DependencyInjection;
+using ProductService.Data;
+using ProductService.IntegrationTests.Fixtures;
+using ProductService.IntegrationTests.Utils;
+using ProductService.Response;
+using System.Net.Http.Json;
+
+namespace ProductService.IntegrationTests.EndpointTests;
+public class ProductEndpointTest : IClassFixture<ProductSvcApplicationFactory>, IAsyncLifetime
+{
+    private readonly ProductSvcApplicationFactory _appFactory;
+
+    private readonly HttpClient _httpClient;
+
+    private const string _validProductId = "3e381e10-4be5-4406-ba23-3bd323b70710";
+
+    public ProductEndpointTest(ProductSvcApplicationFactory appFactory)
+    {
+        _appFactory = appFactory;
+        _httpClient = appFactory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetProducts_ShouldReturn_ProductList()
+    {
+        var response = await _httpClient.GetFromJsonAsync<List<GetProductResponse>>("api/products");
+
+        using var _ = new AssertionScope();
+
+        response.Should().NotBeNull()
+            .And.ContainItemsAssignableTo<GetProductResponse>();
+
+        response.Should().NotBeEmpty()
+            .And.HaveCount(2);
+    }
+
+    [Fact]
+    public async Task GetProductById_WithBaddGuid_ShouldReturn400StatusCode()
+    {
+        var response = await _httpClient.GetAsync("api/products/badguid");
+
+        response.Should().HaveStatusCode(System.Net.HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task GetProductById_WithInvalidId_ShouldReturn404StatusCode()
+    {
+        var response = await _httpClient.GetAsync("api/products/29D70302-AE3F-425F-BEDF-03074847F3D1");
+
+        response.Should().HaveStatusCode(System.Net.HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetProductById_WithValidId_ShouldReturnProduct()
+    {
+        var response = await _httpClient.GetAsync($"api/products/{_validProductId}");
+
+        response.Should().HaveStatusCode(System.Net.HttpStatusCode.OK);
+
+        var product = await response.Content.ReadFromJsonAsync<GetProductResponse>();
+
+        using var _ = new AssertionScope();
+
+        product.Should().NotBeNull();
+
+        product.Should().BeOfType<GetProductResponse>();
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    public Task DisposeAsync()
+    {
+        var scope = _appFactory.Services.CreateScope();
+
+        var db = scope.ServiceProvider.GetRequiredService<ProductDbContext>();
+
+        Database.ReInitializeDb(db);
+
+        return Task.CompletedTask;
+    }
+}
