a953ec9551fa6025e0bcd5b979d1624245390b59
Wale <onafesowale@gmail.com>
2023-11-16 18:05:22 +0100

    added price to object

--- src/ProductService/Request/UpdateProductRequest.cs
+++ src/ProductService/Request/UpdateProductRequest.cs
@@ -5,4 +5,5 @@ public class UpdateProductRequest
     public string Name { get; set; }
     public string Description { get; set; } 
     public string ImageUrl { get; set; }
+    public decimal Price { get; set; }
 }
