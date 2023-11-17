97a45e80c4fb070e4a2970735ab5c1b5afc3eb05
Wale <onafesowale@gmail.com>
2023-11-17 10:59:07 +0100

    Added product updated

---
+++ src/Shared/Contracts/ProductUpdated.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Contracts;
+public class ProductUpdated
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+}
