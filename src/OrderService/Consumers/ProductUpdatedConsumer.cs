417d525d3fc5e75bbb176b2260cf0760739100e5
Wale <onafesowale@gmail.com>
2023-11-17 10:58:22 +0100

    added product updated consumer to order svc

---
+++ src/OrderService/Consumers/ProductUpdatedConsumer.cs
@@ -0,0 +1,39 @@
+﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+using Shared.Contracts;
+
+namespace OrderService.Consumers;
+
+public class ProductUpdatedConsumer : IConsumer<ProductUpdated>
+{
+    private readonly OrderDbContext _dbContext;
+
+    public ProductUpdatedConsumer(OrderDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<ProductUpdated> context)
+    {
+        Guid productId;
+
+        var validGuid = Guid.TryParse(context.Message.Id, out productId);
+
+        if(!validGuid) return;
+
+        var product = await _dbContext.Product
+            .FirstOrDefaultAsync(x => x.Id == productId);
+
+        if (product is null) return;
+
+        product.Name = context.Message.Name;
+        product.Description = context.Message.Description;
+        product.ImageUrl = context.Message.ImageUrl;
+        product.Price = context.Message.Price;
+
+        _dbContext.Product.Update(product);
+
+        await _dbContext.SaveChangesAsync();
+    }
+}
