096597934c5525c2ce757833ae10af27d3bc2ea0
Wale <onafesowale@gmail.com>
2023-11-17 10:58:55 +0100

    added product updated consumer

---
+++ src/SearchService/Consumers/ProductUpdatedConsumer.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using MongoDB.Entities;
+using SearchService.Entities;
+using Shared.Contracts;
+
+namespace SearchService.Consumers;
+
+public class ProductUpdatedConsumer : IConsumer<ProductUpdated>
+{
+    public async Task Consume(ConsumeContext<ProductUpdated> context)
+    {
+        await DB.Update<Product>()
+            .MatchID(context.Message.Id)
+            .Modify(x => x.Name, context.Message.Name)
+            .Modify(x => x.Description, context.Message.Description)
+            .Modify(x => x.ImageUrl, context.Message.ImageUrl)
+            .Modify(x => x.Price, context.Message.Price)
+            .ExecuteAsync();
+    }
+}
