64b1047e4086f5d04d6151e320260fe02dc0fd88
Wale <onafesowale@gmail.com>
2023-11-18 12:10:15 +0100

    IGeneric repository

---
+++ src/OrderService/DataAccess/Contracts/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using OrderService.DataAccess.Specifications;
+
+namespace OrderService.DataAccess.Contracts;
+
+public interface IGenericRepository<T> where T: class
+{
+    Task<T> GetAsync(ISpecification<T> spec);
+    Task<List<T>> GetAllAsync(ISpecification<T> spec);
+    Task AddAsync(T entity);
+
+}
