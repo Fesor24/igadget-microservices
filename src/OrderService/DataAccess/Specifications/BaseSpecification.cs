05742809e090a2382fec3c5b48a508b6abb7ac7b
Wale <onafesowale@gmail.com>
2023-11-18 12:10:49 +0100

    Base specification

---
+++ src/OrderService/DataAccess/Specifications/BaseSpecification.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+namespace OrderService.DataAccess.Specifications;
+
+public class BaseSpecification<T> : ISpecification<T>
+{
+    public BaseSpecification()
+    {
+        
+    }
+
+    public BaseSpecification(Expression<Func<T, bool>> criteria)
+    {
+        Criteria = criteria;        
+    }
+
+    public Expression<Func<T, bool>> Criteria { get; private set; }
+
+    public List<Expression<Func<T, object>>> Includes { get; private set; } = new();
+
+    protected void AddIncludes(Expression<Func<T, object>> include) => 
+        Includes.Add(include);
+}
