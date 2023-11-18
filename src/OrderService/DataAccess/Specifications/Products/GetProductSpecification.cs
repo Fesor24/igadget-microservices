65054093145dbfe937e2cdbbbb33bd8138bd16d2
Wale <onafesowale@gmail.com>
2023-11-18 12:11:33 +0100

    GetProduct specification

---
+++ src/OrderService/DataAccess/Specifications/Products/GetProductSpecification.cs
@@ -0,0 +1,11 @@
+﻿using OrderService.Entities;
+
+namespace OrderService.DataAccess.Specifications.Products;
+
+public class GetProductSpecification : BaseSpecification<Product>
+{
+    public GetProductSpecification(Guid id) : base(x => x.Id == id)
+    {
+        
+    }
+}
