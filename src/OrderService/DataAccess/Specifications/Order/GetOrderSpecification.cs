6d1c14b43afd7b0ca71988291c39c832f7f1f34b
Wale <onafesowale@gmail.com>
2023-11-18 12:11:22 +0100

    GetOrder Specification

---
+++ src/OrderService/DataAccess/Specifications/Order/GetOrderSpecification.cs
@@ -0,0 +1,18 @@
+﻿using OrderEntity = OrderService.Entities.OrderAggregate.Order;
+
+namespace OrderService.DataAccess.Specifications.Order;
+
+public class GetOrderSpecification : BaseSpecification<OrderEntity>
+{
+    public GetOrderSpecification(Guid orderId) : base(x => x.Id == orderId)
+    {
+        AddIncludes(x => x.DeliveryMethod);
+        AddIncludes(x => x.OrderItems);
+    }
+
+    public GetOrderSpecification(string email) : base(x => x.BuyerEmail == email)
+    {
+        AddIncludes(x => x.DeliveryMethod);
+        AddIncludes(x => x.OrderItems);
+    }
+}
