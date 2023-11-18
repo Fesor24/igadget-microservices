87220c3311050205910db2568dc238df011d5fbb
Wale <onafesowale@gmail.com>
2023-11-18 12:10:39 +0100

    Generic repository implementation

---
+++ src/OrderService/DataAccess/Repository/GenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderService.Data;
+using OrderService.DataAccess.Contracts;
+using OrderService.DataAccess.Specifications;
+
+namespace OrderService.DataAccess.Repository;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly OrderDbContext _context;
+
+    public GenericRepository(OrderDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(T entity) =>
+        await _context.Set<T>().AddAsync(entity);
+
+    public async Task<List<T>> GetAllAsync(ISpecification<T> spec) =>
+        await ApplySpecification(spec).ToListAsync();
+
+    public async Task<T> GetAsync(ISpecification<T> spec) =>
+        await ApplySpecification(spec).FirstOrDefaultAsync();
+
+    private IQueryable<T> ApplySpecification(ISpecification<T> spec) =>
+        SpecificationEvaluator.GetQuery(_context.Set<T>().AsQueryable(), spec);
+}
