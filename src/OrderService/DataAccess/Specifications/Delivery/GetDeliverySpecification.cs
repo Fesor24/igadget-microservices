c35ba44038142636d3b43ca84547a8836c39de22
Wale <onafesowale@gmail.com>
2023-11-18 12:11:02 +0100

    get delivery specification

---
+++ src/OrderService/DataAccess/Specifications/Delivery/GetDeliverySpecification.cs
@@ -0,0 +1,16 @@
+﻿using OrderService.Entities.OrderAggregate;
+
+namespace OrderService.DataAccess.Specifications.Delivery;
+
+public class GetDeliverySpecification : BaseSpecification<DeliveryMethod>
+{
+    public GetDeliverySpecification(Guid deliveryId) :  base(x => x.Id == deliveryId)
+    {
+        
+    }
+
+    public GetDeliverySpecification()
+    {
+        
+    }
+}
