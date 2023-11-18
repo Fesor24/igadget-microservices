b18d66d5efcabac6343ae4975f0c0062380af8de
Wale <onafesowale@gmail.com>
2023-11-18 12:11:41 +0100

    specification evaluator

---
+++ src/OrderService/DataAccess/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderService.DataAccess.Specifications;
+public static class SpecificationEvaluator
+{
+    public static IQueryable<T> GetQuery<T>(IQueryable<T> input, ISpecification<T> spec) where T: class
+    {
+        var query = input.AsQueryable();
+
+        if(spec.Criteria is not null)
+        {
+            query = query.Where(spec.Criteria);
+        }
+
+        query = spec.Includes.Aggregate(query, (query, include) => query.Include(include));
+
+        return query;
+    }
+}
