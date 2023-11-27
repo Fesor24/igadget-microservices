8b58e8e6ae5514d480fa9f7fe22fe2b72a6821dd
Wale <onafesowale@gmail.com>
2023-11-27 15:06:46 +0100

    cors policy service

---
+++ src/IdentityService/Services/CorsPolicyService.cs
@@ -0,0 +1,20 @@
+﻿using Duende.IdentityServer.Services;
+
+namespace IdentityService.Services;
+
+public class CorsPolicyService : ICorsPolicyService
+{
+    private readonly IConfiguration _config;
+
+    public CorsPolicyService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public Task<bool> IsOriginAllowedAsync(string origin)
+    {
+        string webClientOrigin = _config["WebHost"];
+
+        return Task.FromResult(webClientOrigin == origin);
+    }
+}
